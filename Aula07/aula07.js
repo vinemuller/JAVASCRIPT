d57eb9c7c552364e1d76c0f80a1ff7bc192f67fc
vinemuller <mullerpadilha051@gmail.com>
2025-06-12 13:07:12 -0300

    Aprendi condições JS

---
+++ Aula07/aula07.js
@@ -0,0 +1,7 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre com atenção!`)
+
