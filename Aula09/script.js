7af21ae92d338afeb4a29a6fd299777418a740b5
vinemuller <mullerpadilha051@gmail.com>
2025-06-16 18:15:03 -0300

    atualizei os ex

--- Aula09/script.js
+++ Aula09/script.js
@@ -4,19 +4,25 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
+    var img = document.createElement('manha.png.png')
+        img.setAttribute('id', 'foto')
     if (hora >= 0 && hora < 12) {
         //Bom dia
-        img.src = 'manha.png' 
+        img.setAttribute('src', 'manha.png.png')
         document.body.style.background ='#afafd6'
+            res.appendchild(img)
+
     } else if (hora >= 12 && hora < 18) {
         //Boa tarde
-        img.src = 'tarde.png' 
+        img.setAttribute('src', 'tarde.png.png')
         document.body.style.background ='#b2da55'
+            res.appendchild(img)
 
     } else {
         //Boa noite
-        img.src = 'noite.png' 
+        img.setAttribute('src', 'noite.png.png')
         document.body.style.background ='#131c72'
+            res.appendchild(img)
 
     }
 }
---
+++ aula10/ambiente.js
@@ -0,0 +1,32 @@
+// EXECUTA PRIMEIRO E TESTA DEPOIS
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+
+
+
+/* TESTA PRIMEIRO EXECUTA DEPOIS
+
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
