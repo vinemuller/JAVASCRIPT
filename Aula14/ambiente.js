2e1c3394984410c344596ea9eaddd59abbb8aff7
vinemuller <mullerpadilha051@gmail.com>
2025-06-19 17:36:51 -0300

    finalizei JS

---
+++ Aula14/ambiente.js
@@ -0,0 +1,16 @@
+let num = [ 5, 8, 2, 9, 3]
+
+num.push(1) //colocar manualmente na ultima posição
+num.sort() // organizar vetores em ordem
+console.log(num)
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(4)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
+
